5b3c156d01fbf5a8635c615e5434a1693cb43709
Muhammet Uçan <mhmmtucan@gmail.com>
2022-06-28 00:04:13 +0200

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+chsh -s $(which zsh)
+
+
+sudo apt-get update
+sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
+sudo apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  openssh-client \
+  less \
+  iproute2 \
+  procps \
+  wget \
+  unzip \
+  apt-transport-https \
+  lsb-release \
+  latexmk
+
+# Cleanup
+sudo apt-get autoremove -y
+sudo apt-get autoremove -y
+sudo rm -rf /var/lib/apt/lists/*
